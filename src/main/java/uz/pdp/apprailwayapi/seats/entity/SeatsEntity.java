fc0634efcf443635ce4263e59c3c913f57964856
Farrukhbek <farruxbekhabibjonov@gmail.com>
2022-02-18 09:01:51 +0500

    created seats vagons trains entity

---
+++ src/main/java/uz/pdp/apprailwayapi/seats/entity/SeatsEntity.java
@@ -0,0 +1,36 @@
+package uz.pdp.apprailwayapi.seats.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.pdp.apprailwayapi.trains.entity.TrainsEntity;
+import uz.pdp.apprailwayapi.wagons.entity.WagonEntity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "seats")
+public class SeatsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private int order_number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wagon_id", referencedColumnName = "id")
+    private WagonEntity wagons;
+
+//    @OneToOne(mappedBy = "seats",
+//            cascade = CascadeType.ALL,
+//            orphanRemoval = true)
+//    private TicketEntity ticket ;
+}
